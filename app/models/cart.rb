9714b615c9762431a19e399f6835cf9ad899aa08
Radek Novak jr <novak.jr@gsystem.sk>
2016-05-15 18:00:44 +0200

    Added button to increment line item quantity in cart (Ajax) and modified decrement button to Ajax-version

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,7 +13,7 @@ class Cart < ActiveRecord::Base
 
   def remove_product(product_id)
     current_item = line_items.find_by(product_id: product_id)
-    if current_item
+    if current_item.quantity > 1
       current_item.quantity -= 1
     else
       current_item.destroy
